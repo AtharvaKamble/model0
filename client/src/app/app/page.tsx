198d6b92e1a7b681a57a939049cfca956620c10e
AtharvaKamble <atharvadkamble00@gmail.com>
2024-06-18 15:30:33 -0400

    [client/feat] add landing page and animations

--- client/src/app/app/page.tsx
+++ client/src/app/app/page.tsx
@@ -5,18 +5,14 @@ import { Button } from "@/components/ui/button";
 import SideBar from "@/components/custom/App/SideBar";
 import Workspace from "@/components/custom/App/Workspace";
 
-interface AppProps {
-  className?: string;
-}
-
 // export const metadata: Metadata = {
 //   title: "Forms",
 //   description: "Advanced form example using react-hook-form and Zod.",
 // };
 
-export default function App({ className }: AppProps) {
+export default function App() {
   return (
-    <section className={`min-h-screen ${className}`}>
+    <section className={`min-h-screen`}>
       <div className="flex">
         <SideBar className="bg-zinc-900 hidden md:block md:w-96 min-h-screen px-4 py-10" />
         <Workspace className="bg-zinc-800 w-full  min-h-screen" />

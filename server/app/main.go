0db81c6de7a1f91f1e86bff277494efeb44a8c20
AtharvaKamble <atharvadkamble00@gmail.com>
2024-07-11 00:56:50 -0400

    [feat] dockerize model0-app

--- server/app/main.go
+++ server/app/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sagemaker"
+)
+
+const (
+	httpAddr = ":8080"
+)
 
 func main() {
-	fmt.Println("")
+	initializeAWS()
+
+	mux := http.NewServeMux()
+
+	handler := NewHandler()
+	handler.RegisterRoutes(mux)
+
+	if err := http.ListenAndServe(httpAddr, mux); err != nil {
+		log.Fatal("Failed to start server")
+	}
+
+}
+
+func initializeAWS() {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+
+	if err != nil {
+		log.Fatal("Error in loading AWS Config: ", err)
+	}
+
+	_ = sagemaker.NewFromConfig(cfg)
+
+	fmt.Println("AWS Sagemaker booted successfully")
+
 }
